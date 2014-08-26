eb24eb1d632aee27a5ba3be231258e6ff0f8d33b
davidlaprade <david.laprade@gmail.com>
2014-08-25 23:25:40 -0400

    found novel way to write algorithm; fixed benchmark call

--- test.rb
+++ test.rb
@@ -1,17 +1,28 @@
-every_combination = %w[a b b b b c d d d].permutation.map &:join
+require 'benchmark'
 
-i = 0
-while i < every_combination.length
-	k = i + 1
-	while k < every_combination.length
-		if every_combination[i] == every_combination[k]
-			every_combination.delete_at(k)
-		else
-			k = k + 1
-		end
-	end
-	i = i + 1
+input = ARGV[0]
+
+def solution(input)
+	every_combination = input.upcase.split("").sort.permutation.map &:join
+	every_combination = every_combination & every_combination
+	return every_combination.index(input.upcase) + 1
 end
 
-print every_combination.length
+# i = 0
+# while i < every_combination.length
+# 	k = i + 1
+# 	while k < every_combination.length
+# 		if every_combination[i] == every_combination[k]
+# 			every_combination.delete_at(k)
+# 		else
+# 			k = k + 1
+# 		end
+# 	end
+# 	i = i + 1
+# end
+
+# print "Number of distinct strings: #{every_combination.length}\n"
+print "Rank of #{input.upcase}: #{solution(input)}\n"
+print Benchmark.measure { solution(input) }
+
 
