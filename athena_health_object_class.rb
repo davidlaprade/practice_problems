e4f8dec0b42d71ae26bce9e644483eca76c0958b
davidlaprade <david.laprade@gmail.com>
2014-08-26 14:26:01 -0400

    added description of problem, had input.char_arry update after calling rank on input

--- athena_health_object_class.rb
+++ athena_health_object_class.rb
@@ -1,3 +1,11 @@
+# ----------------------PROBLEM-----------------------------------------------------------------------------------------------
+# Consider a "word" as any sequence of capital letters A-Z (not limited to just "dictionary words"). For any word with at least two different letters, there are other words composed of the same letters but in a different order (for instance, STATIONARILY/ANTIROYALIST, which happen to both be dictionary words; for our purposes "AAIILNORSTTY" is also a "word" composed of the same letters as these two). 
+# We can then assign a number to every word, based on where it falls in an alphabetically sorted list of all words made up of the same set of letters. 
+# Write a program which takes a word as a command line argument and prints to standard output its number. Do not use the method above of generating the entire list. Your program should be able to accept any word 20 letters or less in length (possibly with some letters repeated), and should use no more than 1 GB of memory and take no more than 500 milliseconds to run. Any answer we check will fit in a 64-bit integer. 
+# Sample words, with their rank: ABAB = 2, AAAB = 1, BAAA = 4, QUESTION = 24572,  BOOKKEEPER = 10743, NONINTUITIVENESS = 8222334634.
+# Your program will be judged on how fast it runs and how clearly the code is written. We will be running your program as well as reading the source code, so anything you can do to make this process easier would be appreciated.
+# -----------------------------------------------------------------------------------------------------------------------------
+
 require 'benchmark'
 
 # since Ruby doesn't contain a factorial method; returns factorial of input, i.e. (input!)
@@ -38,6 +46,7 @@ class Permutation
 
 		# base case: when self.char_array.length == 1
 		if self.char_array.length == 1
+			self.char_array = self.string.upcase.split("")
 			return self.order
 
 		# inductive case: when self.char_array.length > 1
