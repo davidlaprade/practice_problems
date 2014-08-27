bd9b8d8af81723070ffbf175987f88bf9336e595
davidlaprade <david.laprade@gmail.com>
2014-08-26 22:40:52 -0400

    changed method names and comments for clarity, retested algorithm

--- athena_health_object_class.rb
+++ athena_health_object_class.rb
@@ -1,14 +1,19 @@
-# ----------------------PROBLEM-----------------------------------------------------------------------------------------------
-# Consider a "word" as any sequence of capital letters A-Z (not limited to just "dictionary words"). For any word with at least two different letters, there are other words composed of the same letters but in a different order (for instance, STATIONARILY/ANTIROYALIST, which happen to both be dictionary words; for our purposes "AAIILNORSTTY" is also a "word" composed of the same letters as these two). 
+# ----------------------PROBLEM------------------------------------------------------------------------------------------------------------------------
+# Consider a "word" as any sequence of capital letters A-Z (not limited to just "dictionary words"). For any word with at least two different letters, 
+# there are other words composed of the same letters but in a different order (for instance, STATIONARILY/ANTIROYALIST, which happen to both be 
+# dictionary words; for our purposes "AAIILNORSTTY" is also a "word" composed of the same letters as these two). 
 # We can then assign a number to every word, based on where it falls in an alphabetically sorted list of all words made up of the same set of letters. 
-# Write a program which takes a word as a command line argument and prints to standard output its number. Do not use the method above of generating the entire list. Your program should be able to accept any word 20 letters or less in length (possibly with some letters repeated), and should use no more than 1 GB of memory and take no more than 500 milliseconds to run. Any answer we check will fit in a 64-bit integer. 
+# Write a program which takes a word as a command line argument and prints to standard output its number. Do not use the method above of generating 
+# the entire list. Your program should be able to accept any word 20 letters or less in length (possibly with some letters repeated), and should use 
+# no more than 1 GB of memory and take no more than 500 milliseconds to run. Any answer we check will fit in a 64-bit integer. 
 # Sample words, with their rank: ABAB = 2, AAAB = 1, BAAA = 4, QUESTION = 24572,  BOOKKEEPER = 10743, NONINTUITIVENESS = 8222334634.
-# Your program will be judged on how fast it runs and how clearly the code is written. We will be running your program as well as reading the source code, so anything you can do to make this process easier would be appreciated.
-# -----------------------------------------------------------------------------------------------------------------------------
+# Your program will be judged on how fast it runs and how clearly the code is written. We will be running your program as well as reading the source 
+# code, so anything you can do to make this process easier would be appreciated.
+# -----------------------------------------------------------------------------------------------------------------------------------------------------
 
 require 'benchmark'
 
-# since Ruby doesn't contain a factorial method; returns factorial of input, i.e. (input!)
+# returns factorial of input, i.e. (input!); Ruby doesn't contain a factorial
 def factorial(input)
 	if input == 0
 		return 1
@@ -23,49 +28,56 @@ class Array
 		length = self.length
 		# get array of unique members
 		uniq = self & self
-		# incase there are repititious characters in the input
+
+		# when there are no repeating characters, the number of unique permutations of a string == the factorial of that string's length
+		# but when there are repeating letters, there are fewer unique permutations of the string
+		# for example, there are 24 unique permutations of "ABCD" but only 12 unique permutations of "AACD"--though they have the same length
+		# when there are repetitions of characters, you can find the number of unique permutations by taking the factorial of the string length
+		# and dividing it by the product of [for each unique character in the string, the factorial of the number of times it occurs in the string]--no easy way to say it!
 		uniq.map! { |x| factorial(self.count(x)) }
 		return factorial(length)/uniq.inject(:*)
 	end
 end
 
 class Permutation
-	attr_accessor :string, :order, :char_array
+	attr_accessor :string, :rank, :char_array
 
 	def initialize(string)
 		@string = string
 		@char_array = string.upcase.split("")
-		@order = 1
+		@rank = 1
 	end
 
-	# called on Permutation object, returns alphabetical rank of Permutation.string
-	# among all other strings containing the same letters
-	def rank
+	# recursive method called on Permutation object
+	# returns alphabetical rank of Permutation.string among all other strings containing the same letters
+	def calculate_rank
 		# creates bank of unique characters in self.char_array, then sorts them alphabetically
-		letter_bank = (self.char_array&self.char_array).sort
+		letter_bank = (self.char_array & self.char_array).sort
 
 		# base case: when self.char_array.length == 1
 		if self.char_array.length == 1
+			# reset the char_array attribute
 			self.char_array = self.string.upcase.split("")
-			return self.order
+			# terminate the loop
+			return self.rank
 
 		# inductive case: when self.char_array.length > 1
 		else
 			# when the char_array does NOT begin with the first-ranked letter in the letter_bank
 			# we know that self.string is alphabetically behind each unique string that starts with the first-ranked letter
-			# so, add the number of said unique strings to self.order
+			# so, add the number of unique strings self.string is alphabetically behind to self.rank
 			while self.char_array.first != letter_bank.first
 				dup_array = self.char_array.dup
-				# find out how many unique strings begining with the first-ranked letter 
-				# can be generated from the letters in self.char_array, add this to the char_array's rank
+				# find out how many unique strings begining with the first-ranked letter can be generated 
+				# from the letters in self.char_array, add this to the char_array's rank
 				dup_array.delete_at( dup_array.index(letter_bank.first) || dup_array.length)
-				self.order = self.order + dup_array.count_uniq_permutations
+				self.rank = self.rank + dup_array.count_uniq_permutations
 				# now repeat the process above until there are no letters in the letter_bank ranked before self.char_array.first
 				letter_bank.shift
 			end
 			# now repeat for the next character in the char_array
 			self.char_array.shift
-			self.rank
+			self.calculate_rank
 		end
 	end
 
@@ -76,7 +88,7 @@ end
 input = Permutation.new(ARGV[0])
 
 # passes formatted results to standard out
-print "Rank of #{input.string}: #{input.rank}\n"
-print "Run time: %f seconds\n" % Benchmark.realtime { input.rank }.to_f
+print "Rank of #{input.string}: #{input.calculate_rank}\n"
+print "Run time: %f seconds\n" % Benchmark.realtime { input.calculate_rank }.to_f
 pid, size = `ps ax -o pid,rss | grep -E "^[[:space:]]*#{$$}"`.strip.split.map(&:to_i)
 print "Memory used: %f GB\n" % (size/1000000).to_f
--- athena_health_spec.rb
+++ athena_health_spec.rb
@@ -1,106 +1,107 @@
 $LOAD_PATH << '.'
-require 'athena_health'
+require 'athena_health_object_class'
+# require 'athena_health'
 # require 'test'
 
 describe "count_uniq_permutations(array)" do
 
 	it "should output 1 when passed A" do
 		input = "A".split("")
-		expect(count_uniq_permutations(input)).to eq(1)
+		expect(input.count_uniq_permutations).to eq(1)
 	end
 
 	it "should output 1 when passed AAAAAAAAAAAAAA" do
 		input = "AAAAAAAAAAAAAA".split("")
-		expect(count_uniq_permutations(input)).to eq(1)
+		expect(input.count_uniq_permutations).to eq(1)
 	end
 
 	it "should output 14 when passed AAAAAAAAAAAAAB" do
 		input = "AAAAAAAAAAAAAB".split("")
-		expect(count_uniq_permutations(input)).to eq(14)
+		expect(input.count_uniq_permutations).to eq(14)
 	end
 
 	it "should output 90 when passed AABBCC" do
 		input = "AABBCC".split("")
-		expect(count_uniq_permutations(input)).to eq(90)
+		expect(input.count_uniq_permutations).to eq(90)
 	end
 
 	it "should output 1680 when passed AAABBBCCC" do
 		input = "AAABBBCCC".split("")
-		expect(count_uniq_permutations(input)).to eq(1680)
+		expect(input.count_uniq_permutations).to eq(1680)
 	end
 
 	# should output the same thing as previous when input letters are scrambled
 	it "should output 1680 when passed BCABACABC" do
 		input = "BCABACABC".split("")
-		expect(count_uniq_permutations(input)).to eq(1680)
+		expect(input.count_uniq_permutations).to eq(1680)
 	end
 
 	it "should output 7821830016000 when passed AAAAAABBBCCCDEEEFGHH" do
 		input = "AAAAAABBBCCCDEEEFGHH".split("")
-		expect(count_uniq_permutations(input)).to eq(7821830016000)
+		expect(input.count_uniq_permutations).to eq(7821830016000)
 	end
 
 end
 
 
 
-describe "rank" do
+describe "calculate_rank" do
 	# it should work when there are no repeated letters
 	it "should return 21 when passed DBAC" do
-		input = "DBAC".split("")
-		expect(rank(input,1)).to eq(21)
-		expect(Benchmark.realtime { rank(input,1) }).to be < 0.5
+		input = Permutation.new("DBAC")
+		expect(input.calculate_rank).to eq(21)
+		expect(Benchmark.realtime { input.calculate_rank }).to be < 0.5
 	end
 
 	# it should work when there are repeated letters
 	it "should return 2 when passed ABAB" do
-		input = "ABAB".split("")
-		expect(rank(input,1)).to eq(2)
-		expect(Benchmark.realtime { rank(input,1) }).to be < 0.5
+		input = Permutation.new("ABAB")
+		expect(input.calculate_rank).to eq(2)
+		expect(Benchmark.realtime { input.calculate_rank }).to be < 0.5
 	end
 	
 	it "should return 1 when passed AAAAAAAAAAAAAAAAAAAAAAAAAAAA" do
-		input = "AAAAAAAAAAAAAAAAAAAAAAAAAAAA".split("")
-		expect(rank(input,1)).to eq(1)
-		expect(Benchmark.realtime { rank(input,1) }).to be < 0.5
+		input = Permutation.new("AAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+		expect(input.calculate_rank).to eq(1)
+		expect(Benchmark.realtime { input.calculate_rank }).to be < 0.5
 	end
 
 	it "should return 1 when passed AAAB" do
-		input = "AAAB".split("")
-		expect(rank(input,1)).to eq(1)
-		expect(Benchmark.realtime { rank(input,1) }).to be < 0.5
+		input = Permutation.new("AAAB")
+		expect(input.calculate_rank).to eq(1)
+		expect(Benchmark.realtime { input.calculate_rank }).to be < 0.5
 	end
 
 	it "should return 4 when passed BAAA" do
-		input = "BAAA".split("")
-		expect(rank(input,1)).to eq(4)
-		expect(Benchmark.realtime { rank(input,1) }).to be < 0.5
+		input = Permutation.new("BAAA")
+		expect(input.calculate_rank).to eq(4)
+		expect(Benchmark.realtime { input.calculate_rank }).to be < 0.5
 	end
 
 
 	it "should return 24572 when passed QUESTION" do
-		input = "QUESTION".split("")
-		expect(rank(input,1)).to eq(24572)
-		expect(Benchmark.realtime { rank(input,1) }).to be < 0.5
+		input = Permutation.new("QUESTION")
+		expect(input.calculate_rank).to eq(24572)
+		expect(Benchmark.realtime { input.calculate_rank }).to be < 0.5
 	end
 
 	it "should return 10743 when passed BOOKKEEPER" do
-		input = "BOOKKEEPER".split("")
-		expect(rank(input,1)).to eq(10743)
-		expect(Benchmark.realtime { rank(input,1) }).to be < 0.5
+		input = Permutation.new("BOOKKEEPER")
+		expect(input.calculate_rank).to eq(10743)
+		expect(Benchmark.realtime { input.calculate_rank }).to be < 0.5
 	end
 
 	it "should return 8222334634 when passed NONINTUITIVENESS" do
-		input = "NONINTUITIVENESS".split("")
-		expect(rank(input,1)).to eq(8222334634)
-		expect(Benchmark.realtime { rank(input,1) }).to be < 0.5
+		input = Permutation.new("NONINTUITIVENESS")
+		expect(input.calculate_rank).to eq(8222334634)
+		expect(Benchmark.realtime { input.calculate_rank }).to be < 0.5
 	end
 
 	# it should return the same as before when capitolization is changed
 	it "should return 8222334634 when passed NoNINTUitIVEnesS" do
-		input = "NoNINTUitIVEnesS".split("")
-		expect(rank(input,1)).to eq(8222334634)
-		expect(Benchmark.realtime { rank(input,1) }).to be < 0.5
+		input = Permutation.new("NoNINTUitIVEnesS")
+		expect(input.calculate_rank).to eq(8222334634)
+		expect(Benchmark.realtime { input.calculate_rank }).to be < 0.5
 	end
 
 
